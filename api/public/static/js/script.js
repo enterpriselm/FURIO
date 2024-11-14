3a5b2c0070b4752a5b1e43bc20022f3e56ce1c85
barrosyan <yanbarrosyan@gmail.com>
2024-11-14 15:33:41 -0300

    FIX: Path for css and js

--- api/public/static/js/script.js
+++ api/public/static/js/script.js
@@ -1,4 +1,4 @@
-const apiUrl = process.env.API_URL || 'http://localhost:8000';
+const apiUrl = 'https://furio.onrender.com/' || 'http://localhost:8000';
 
 document.getElementById('upload-form').addEventListener('submit', async (event) => {
     event.preventDefault();
